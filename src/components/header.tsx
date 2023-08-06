37b7756c1c91328608b884112956f8cbed9e7f81
TuanNT <tuan@wearebloom.com.au>
2023-08-06 21:03:34 +0700

    FIx logo alt

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
 
       <nav className="container flex justify-between items-center">
         <div className="hidden lg:flex w-20 py-5">
-          <a href="#home"> <img className="hidden lg:flex lg:w-20 " src="./logo192.png" /></a>
+          <a href="#home"> <img className="hidden lg:flex lg:w-20 " src="./logo192.png" alt="Logo" /></a>
         </div>
         <div>
           <ul className="hidden lg:flex items-center space-x-6">
